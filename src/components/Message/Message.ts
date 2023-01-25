97ba5aef489985afee85c7601bd5aede23743e7c
tayru <denis.brudanin@mail.ru>
2023-01-26 01:01:36 +0300

    вывод сообщений через сокеты

---
+++ src/components/Message/Message.ts
@@ -0,0 +1,30 @@
+import Block from '../../core/Block';
+import template from 'bundle-text:./template.hbs';
+
+
+interface ChatItemProps {
+  id: string;
+  title: string;
+  last_message: string;
+  unread_count: string;
+  onClick: (event: Event  | undefined) => void;
+  events?: { [key: string]: (event?: Event) => void}
+}
+export class Message extends Block<ChatItemProps> {
+// export class Button extends Block {
+  static componentName = `Message`;
+
+  constructor(props: ChatItemProps) {
+    super({
+      ...props,
+      events: {
+        click: props.onClick
+      }
+    });
+  }
+
+  protected render(): string {
+    return template;
+  }
+}
+
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,7 @@ export { default as Link } from './link';
 
 export { default as Chat } from './messenger/chat';
 export { default as chatItem } from './chatItem';
+export { default as Message } from './Message';
 
 export { default as changeChat } from './messenger/change-chat';
 export { default as textArea } from './textArea';
