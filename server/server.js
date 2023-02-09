18b3bbbaadcf6c3a361b4ba49b12bf4f6a29d1ce
DenisBrudanin <denis.brudanin@neuro-city.ru>
2023-02-09 12:08:25 +0300

    обновление сервера

--- server/server.js
+++ server/server.js
@@ -4,9 +4,10 @@ const app = express();
 const port = 3000;
 
 app.use(express.static('dist'));
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '/dist/index.html'));
-});
+app.use("/", express.static(path.join("dist")))
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve("dist", "index.html"))
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
